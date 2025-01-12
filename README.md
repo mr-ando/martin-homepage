5220e19692d299c62bde46299882a1d5df9bd307
Martin Rui Andorsen <mndorsen@gmail.com>
2025-01-12 18:09:34 +0100

    start work on bloglist

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## Astro Based Static Web App
+
+# Features to create:
+
+- Articles
+
+- Book recommendations
+
+- Video player
---
+++ src/blogposts/blogs.ts
@@ -0,0 +1,9 @@
+interface blog {
+  number: number;
+  title: string;
+}
+
+export const blogs: blog[] = [
+  { number: 1, title: "Blog 1" },
+  { number: 2, title: "Blog 2" },
+];
