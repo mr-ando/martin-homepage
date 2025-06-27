22f5881ac120f1a803da9ac86bf0b8eadd20a874
Martin Andorsen <martin.andorsen@crayon.no>
2025-06-27 12:11:51 +0200

    Add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,15 @@ import tailwind from '@astrojs/tailwind';
 
 
 
+import node from '@astrojs/node';
+
+
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: node({
+    mode: 'standalone'
+  })
 });
